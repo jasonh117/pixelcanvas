ddc4cecb4b6dafdc3e1f1d0a61975a33885cc047
Jason Hsieh <jasonh117@hotmail.com>
2017-07-05 10:57:21 -0700

    initial express setup

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const nconf = require('nconf');
+const winston = require('winston');
+
+const app = express();
+
+const env = (process.env.NODE_ENV !== undefined) ? process.env.NODE_ENV.trim() : 'development';
+
+nconf.argv()
+   .env()
+   .file({ file: `${__dirname}/config.${env}.json` });
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set up routes
+const appRoutes = nconf.get('routes');
+appRoutes.map((item) => {
+  app.use(item.url, require(`${__dirname}/${item.file}`));
+  winston.log('info', 'Loaded route', item);
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
